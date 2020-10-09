4acc82654b8eb0be4acdb52bb93d5c96a767e0e9
Bhumin Bhandari <bhandaribhumin@gmail.com>
2020-10-09 13:00:15 +0530

    add scss globaly

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,6 @@
 import { Config } from '@stencil/core';
+import { version } from './package.json';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'reusable-ui',
@@ -14,8 +16,27 @@ export const config: Config = {
       type: 'docs-readme',
     },
     {
+      type: 'docs-custom',
+      generator: (docs: any) => {
+        docs = Object.assign(docs, { version });
+      }
+    },
+    {
+      type: 'docs-json',
+      file: './dist/components.json'
+    },
+    {
+      type: 'docs-vscode',
+      file: './dist/custom-elements.json'
+    },
+    {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
   ],
+  plugins: [
+    sass({
+      includePaths: ['./node_modules', './src/global-styles']
+    })
+  ]
 };
